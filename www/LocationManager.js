f3eb6109e4f53f9a5f85b7d856645e5312601328
Quentin Fung <31120177+QuentinFung@users.noreply.github.com>
2018-04-27 12:42:18 +0800

    update clobber

--- www/LocationManager.js
+++ www/LocationManager.js
@@ -554,5 +554,5 @@ locationManager.CircularRegion = CircularRegion;
 locationManager.BeaconRegion = BeaconRegion;
 locationManager.Delegate = Delegate;
 
-module.exports.LocationManager = LocationManager;
+module.exports = LocationManager;
 module.exports.locationManager = locationManager;
